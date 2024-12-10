824e4a7a08cbb11968152672bd26b62c61d9a6aa
Estados <hsuliman@gmail.com>
2024-12-10 17:41:40 +0000

    Class MathCommand finish

--- src/main/java/Software/ulpgc/kata6/commands/MathCommand.java
+++ src/main/java/Software/ulpgc/kata6/commands/MathCommand.java
@@ -1,9 +1,25 @@
 package Software.ulpgc.kata6.commands;
 
+import java.util.Locale;
+
 public class MathCommand implements Command{
 
     @Override
     public int execute(String operation, int a, int b){
-        return 0;
+        switch (operation.toLowerCase()){
+            case "add":
+                return a + b;
+            case "subtract":
+                return a - b;
+            case "multiply":
+                return a * b;
+            case "divison":
+                if(b == 0){
+                    throw new ArithmeticException("El dividendo no puede ser 0");
+                }
+                return a / b;
+            default:
+                throw new IllegalArgumentException("Operacion invalida");
+        }
     }
 }
