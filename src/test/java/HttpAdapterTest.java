050ddae477564e6d3df8fd11005a2b9725b2ca83
Estados <hsuliman@gmail.com>
2024-12-10 17:52:23 +0000

    Test HttpAdapterTest finish

--- src/test/java/HttpAdapterTest.java
+++ src/test/java/HttpAdapterTest.java
@@ -9,4 +9,22 @@ public class HttpAdapterTest {
         String result = HttpAdapter.handle("add", "2", "5");
         assertEquals("7", result);
     }
+
+    @Test
+    void TestHandleSubtract(){
+        String result = HttpAdapter.handle("subtract", "7", "5");
+        assertEquals("2", result);
+    }
+
+    @Test
+    void TestHandleMultiply(){
+        String result = HttpAdapter.handle("multiply", "2", "5");
+        assertEquals("10", result);
+    }
+
+    @Test
+    void TestHandleDivide(){
+        String result = HttpAdapter.handle("divide", "10", "2");
+        assertEquals("5", result);
+    }
 }
